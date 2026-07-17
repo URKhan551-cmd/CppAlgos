f8d55e094498030be007830af584de566e14cc6c
UR Rehman <umair.rehman221@gmail.com>
2026-07-17 14:11:20 +0400

    Add number pattern printing in numbers.cpp
    Implement a pattern printing program that displays numbers in increasing order for each line.

---
+++ Pattrern-in-CPP/numbers.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 4;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << i;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+// outputt
+// 1
+// 12
+// 123
+// 1234
