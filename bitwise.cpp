00571f46daab377b55d77d4b8072292131a7c5c3
UR Rehman <umair.rehman221@gmail.com>
2026-07-21 14:30:40 +0400

    Implement bitwise operations in bitwise.cpp
    Added bitwise operations demonstration in main function.

---
+++ bitwise.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+     
+    // int a = 4, b = 8;
+//     cout<< (a & b) <<endl; //0
+//    cout<< ( a | b) << endl; // 12 in decimal  binary = 1100
+//    cout<<(a ^ b) << endl; // decimal 12
+
+int a =8, b=6;
+   cout<< (a & b) <<endl;  // 2
+   cout<< ( a | b) << endl;  // 7
+   cout<< ( a | b) << endl; // 7
+
+
+   cout<< (4 << 1)<< endl;   // 8
+   cout<<(10 << 2)<<endl;    // 40
+   cout<<(10 >> 1)<< endl; // 5
+   cout<<(10 >> 2)<< endl; // 2
+   cout<<(4 >> 2)<< endl; // binary 001 decimal becomes 1
+   cout<<(a * (2))<<endl; // 16
+ cout<<(8 >> 2)<<endl; // 
+ 
+
+
+
+    return 0;
+}
