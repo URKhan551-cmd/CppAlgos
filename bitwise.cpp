4e42fe1e956cbde136e9eeca9bd690efd3b9bd1f
UR Rehman <umair.rehman221@gmail.com>
2026-07-21 14:34:31 +0400

    Enhance bitwise.cpp with additional output examples
    Added new output statements for bitwise operations.

--- bitwise.cpp
+++ bitwise.cpp
@@ -21,6 +21,14 @@ int a =8, b=6;
    cout<<(4 >> 2)<< endl; // binary 001 decimal becomes 1
    cout<<(a * (2))<<endl; // 16
  cout<<(8 >> 2)<<endl; // 
+     cout<<"yaha se new start howa"<<endl;
+
+ cout<<(6 & 10)<<endl;  // 2
+ cout<<(6 | 10)<<endl;  // 14
+ cout<<(6 ^ 10)<<endl;  // 12
+
+ cout<<(10<<2)<<endl;  //40
+ cout<<(10>>1)<<endl; // 5
  
 
 
